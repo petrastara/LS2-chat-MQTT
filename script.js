3236e42a6b0d03df8d1d2917f473e9ceefc2250f
Petra Stará <petrastara1@seznam.cz>
2022-08-03 08:53:07 +0200

    po zkopírování

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+client = new Paho.MQTT.Client("d57a0d1c39d54550b147b58411d86743.s2.eu.hivemq.cloud", 8884, "mqttsend");
+
+client.connect({ /*událost, podařilo se připojit = onSuccess, reaguji na ní fcí, která mě zajímá*/
+    onSuccess: onConnect,
+    userName: "robot",
+    password: "P@ssW0rd!",
+    useSSL: true
+});
+
+
+function onConnect() {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("onConnect");
+    message = new Paho.MQTT.Message("Ahoj"); /*CO posílám*/
+    message.destinationName = "/row/13/text"; /*kam to posílám ????????*/ 
+    client.send(message); /*posílání zprávy*/
+
+    client.onMessageArrived = onMessageArrived; /*říci, že mě ta událost zajímá*/
+    client.subscribe("bridge/temperature"); /*zapsání se k odběru zpráv*/
+}
+
+function onMessageArrived(message) { 
+    console.log("onMessageArrived:" + message.destinationName); /*kam ta zpráva dorazila + hodnota*/
+    console.log("onMessageArrived:" + message.payloadString);   /* payload = obsah zprávy*/
+    /*jak to má být:*/ document.getElementById("teplota").innerText= message.payloadString;
+        
+    /* let zobrazTeplotu = document.querySelector("#teplota");
+    zobrazTeplotu.textContent=message.payloadString;*/
+}
+
+function sendMessage(){
+    message = new Paho.MQTT.Message(document.getElementById("payload").value);  /*value = obsah textového pole*/
+    message.destinationName = "/row/13/text"; 
+    client.send(message); 
+}
